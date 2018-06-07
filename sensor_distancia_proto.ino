7f85b6f2629c7a2ba30afa3a08d207375be17fa4
Rodrigo Hanuch <rihanuch@uc.cl>
2018-06-06 20:55:56 -0400

    update comment

--- sensor_distancia_proto.ino
+++ sensor_distancia_proto.ino
@@ -1,7 +1,7 @@
 #include <HCSR04.h>
 
 UltraSonicDistanceSensor distanceSensor_1(13, 12);  // Initialize sensor that uses digital pins 13 and 12.
-UltraSonicDistanceSensor distanceSensor_2(11, 10);  // Initialize sensor that uses digital pins 13 and 12.
+UltraSonicDistanceSensor distanceSensor_2(11, 10);  // Initialize sensor that uses digital pins 11 and 10.
 
 void setup () {
     Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
